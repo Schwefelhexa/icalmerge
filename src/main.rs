944c4dcdb9c7d1cce7357f4faee9db89f272ddb1
Alexander Baron <alexander.baron1@rwth-aachen.de>
2024-02-16 15:24:17 +0100

    Mirror calendar events

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,57 @@
 use std::{env, io::Cursor};
 
-use ics::{properties::Name, ICalendar};
+use ical::IcalParser;
+use ics::{components::Property, properties::Name, ICalendar};
 use itertools::Itertools;
-use log::info;
 use rocket::{get, http::ContentType, launch, response::Responder, routes, Response};
 
 #[get("/")]
-fn index() -> Cal<'static> {
-    let mut cal = ICalendar::new("2.0", format!("-//Alexander Baron//iCal merge v{}//EN", env!("CARGO_PKG_VERSION")));
-    cal.push(Name::new("Merged Calendar"));
+async fn index() -> Cal<'static> {
+    let mut output_calendar = ICalendar::new(
+        "2.0",
+        format!(
+            "-//Alexander Baron//iCal merge v{}//EN",
+            env!("CARGO_PKG_VERSION")
+        ),
+    );
+    output_calendar.push(Name::new("Merged Calendar"));
 
     let sources_raw = env::var("ICAL_SOURCES").unwrap_or_default();
     let sources = sources_raw.split(',').collect_vec();
-    info!("Sources: {:?}", sources);
 
-    Cal(cal)
+    for source in sources {
+        let ical = reqwest::get(source).await.unwrap().text().await.unwrap();
+        let ical = IcalParser::new(ical.as_bytes());
+
+        for calendar in ical.flatten() {
+            for event in calendar.events {
+                let uid = event
+                    .properties
+                    .iter()
+                    .find(|p| p.name.to_uppercase() == "UID")
+                    .and_then(|p| p.value.clone())
+                    .unwrap();
+                let dtstamp = event
+                    .properties
+                    .iter()
+                    .find(|p| p.name.to_uppercase() == "DTSTAMP")
+                    .and_then(|p| p.value.clone())
+                    .unwrap();
+                let mut output_event = ics::Event::new(uid, dtstamp);
+
+                for prop in event.properties {
+                    output_event.push(Property::new(
+                        prop.name.clone(),
+                        prop.value.clone().unwrap_or("".to_string()),
+                    ));
+                }
+
+                output_calendar.add_event(output_event);
+            }
+        }
+    }
+
+    Cal(output_calendar)
 }
 
 #[launch]
